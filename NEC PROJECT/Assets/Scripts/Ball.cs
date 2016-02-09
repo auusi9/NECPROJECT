c04293c6b00c25879877033ccff6a3e91c7f21ad
AlexisCosano <alcoro13@gmail.com>
2016-02-10 00:45:26 +0100

    Power up speed done :+1: :+1: :+1:

--- NEC PROJECT/Assets/Scripts/Ball.cs
+++ NEC PROJECT/Assets/Scripts/Ball.cs
@@ -8,23 +8,49 @@ public class Ball : MonoBehaviour {
     public CircleCollider2D ball_collider;
     public PolygonCollider2D spike;
     public PolygonCollider2D power_up_speed;
+    public Vector3 ball_position;
+    float actual_time;
+    float speed;
+    bool active;
 	// Use this for initialization
 	void Start () 
     {
         ball.AddForce(new Vector3(80.0f, 0.0f, 0.0f));
+        speed = 5.0f;
 	}
 	
+    void Reset()
+    {
+        ball.position = ball_position;
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-        ball.velocity = ball.velocity.normalized * 5.0f;
+        ball.velocity = ball.velocity.normalized * speed;
+        if(active == true)
+        {
+            speed = 10.0f;
+            ball.AddForce(new Vector3(80.0f, 0.0f, 0.0f));
+            if((Time.time - actual_time) > 2.0f)
+            {
+                active = false;
+                speed = 5.0f;
+            }
+        }
+        
         if(ball_collider.IsTouching(portal))
         {
             ball.velocity = Vector3.zero;
         }
         else if(ball_collider.IsTouching(spike))
         {
-            ball.velocity = Vector3.zero;
-        }            
+            Reset();
+        }
+        else if(ball_collider.IsTouching(power_up_speed))
+        {
+            actual_time = Time.time;
+            active = true;
+        }
 	}
 }
