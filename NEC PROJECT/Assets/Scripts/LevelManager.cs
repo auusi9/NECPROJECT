db18924eb691758ab932801c9f68471555f9d5f1
kellazo <gilli11519@gmail.com>
2016-02-12 18:47:17 +0100

    UI Start Menu
    Under construction....
    Script Level Manager for all levels
    Script Start Level for initial level

---
+++ NEC PROJECT/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ NEC PROJECT/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+
+    //public Canvas quitMenu;
+    public Button playText;
+    //public Button CustomizeText;
+    //public Button SettingsText;
+    //public Button StatsText;
+    //public Button exitText;
+    //public string LevelName;
+
+    // Use this for initialization
+    void Start()
+    {
+      //  quitMenu = quitMenu.GetComponent<Canvas>();
+        playText = playText.GetComponent<Button>();
+       // quitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        //quitMenu.enabled = true;
+        playText.enabled = false;
+        //exitText.enabled = false;
+    }
+    public void Nopress()
+    {
+        //quitMenu.enabled = false;
+        playText.enabled = true;
+        //exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("NECSCENE");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
